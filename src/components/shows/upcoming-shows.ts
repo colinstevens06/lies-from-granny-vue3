161584ae2545a674784230ecf0bd47fe19d0cb3d
Colin Stevens <colinstevens06@gmail.com>
2023-08-24 17:46:56 -0400

    lfg-0004 show & songs update

--- src/components/shows/upcoming-shows.ts
+++ src/components/shows/upcoming-shows.ts
@@ -1,17 +1,3 @@
 import type { Gig } from '@models/gig';
 
-export const upcomingShows = [
-	{
-		id: 15,
-		name: '#FightLikeFitz',
-		date: 'July 29, 2023',
-		text: [
-			'Come join us a the 8x10 for a benefit show!',
-			'Ticket sales will go to friend-of-the-band Tommy Fitzgerald to help in his fight against ALS.',
-			'We have a fresh set ready, so come ready to dance and sing all night.',
-			'Hope to see you there!'
-		],
-		img: '/images/shows/8x10-2023-07-29.jpg',
-		imgAlt: 'FightLikeFitz - July 29, 2023'
-	}
-] as Gig[];
+export const upcomingShows = [] as Gig[];
