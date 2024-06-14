0a7d38404417d21306a76848059a190f8c7410a7
Colin Stevens <colinstevens06@gmail.com>
2024-06-13 20:13:12 -0400

    lfg-2024 fran updates

--- src/components/home/main-text.vue
+++ src/components/home/main-text.vue
@@ -2,26 +2,26 @@
 	<div class="main-container my-5">
 		<h2>High-Energy Pop Rock Cover Band</h2>
 		<p>
-			Lies From Granny is a five-piece rock cover band who brings a party environment while playing a mix of rock, pop
-			punk and anything else you might want to dance to.
+			Lies From Granny is a five-piece pop rock cover band from the Washington D.C. - Baltimore area who always brings
+			the party to every stage they hit
 		</p>
 		<p>
-			We love learning new songs and are always adding new tunes to our arsenal. So anytime you see us, expect to see a
-			different show than last time.
+			Come out to a Lies From Granny show and you'll be dancing and singing along to your favorite songs all night long.
+			We play covers of popular rock hits from the late 90's and early 2000's that you're sure to enjoy, and we're known
+			to throw fun tunes from other decades in the mix too.
 		</p>
 		<p>
-			You can find us at venues in the Baltimore and D.C. areas. Have a song you think we should learn? A place we
-			should play? Drop us a note on
-			<a href="https://www.instagram.com/liesfromgranny/" target="_blank" rel="noopener noreferrer">Instagram</a>
-			or
-			<!-- prettier-ignore -->
-			<span><a :href="urls.facebook" target="_blank" rel="noopener noreferrer">Facebook</a>.</span>
+			You can find us rocking out at venues across D.C., Maryland and Virginia. We'd love to see you at a show! Follow
+			us on social media to see where we'll be next, or
+			<router-link to="/contact">book us for your next event</router-link>
 		</p>
-		<p class="mb-3">
-			Want to book us for a show?
-			<!-- prettier-ignore -->
-			<span><router-link to="/contact">We'd love to hear from you</router-link>.</span>
-		</p>
+		<p class="font-bold">Follow Us!</p>
+		<ul class="list-none pl-0">
+			<li>
+				<a href="https://www.instagram.com/liesfromgranny/" target="_blank" rel="noopener noreferrer">Instagram</a>
+			</li>
+			<li class="mt-1"><a :href="urls.facebook" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+		</ul>
 	</div>
 </template>
 
